033cd192a8327f816f622763e1ce278e1dfa18c9
YogiramV <yogiramkv@gmail.com>
2025-01-28 14:48:31 +0530

    Modify the implementation

--- app.py
+++ app.py
@@ -1,24 +1,17 @@
-import os
 from main import scan_photo, load_encodings
-from image_encoder import encode_faces_once
 from PIL import Image
 import io
 
+known_face_encodings, known_face_rollnos, known_face_names = [], [], []
 
 def process_face_recognition(captured_image, rollno_input):
+    global known_face_encodings, known_face_rollnos, known_face_names
 
     known_face_encodings, known_face_rollnos, known_face_names = load_encodings()
 
     image_bytes = captured_image.getvalue()
-
     img = Image.open(io.BytesIO(image_bytes))
 
-    temp_image_path = "temp_uploaded_image.jpg"
-    img.save(temp_image_path)
-
-    result = scan_photo(temp_image_path, rollno_input,
-                        known_face_encodings, known_face_rollnos, known_face_names)
-
-    os.remove(temp_image_path)
+    result = scan_photo(img, rollno_input, known_face_encodings, known_face_rollnos, known_face_names)
 
     return result
--- submain.py
+++ submain.py
@@ -1,29 +1,37 @@
 from flask import Flask, request, jsonify
 from io import BytesIO
 from app import process_face_recognition
-from image_encoder import *
+from image_encoder import encode_faces_once
+import os
 
 app = Flask(__name__)
 
 initialized = False
-
+ENCODED_FACES_TIMESTAMP_FILE = "encoded_faces_timestamp.txt"
 
 def deploy_initialization():
     encode_faces_once()
 
+def check_and_initialize_encoding():
+    global initialized
+    last_encoded_time = os.path.getmtime('Faces')
+
+    if not os.path.exists(ENCODED_FACES_TIMESTAMP_FILE) or \
+       last_encoded_time > float(open(ENCODED_FACES_TIMESTAMP_FILE).read()):
+        deploy_initialization()
+        with open(ENCODED_FACES_TIMESTAMP_FILE, 'w') as f:
+            f.write(str(last_encoded_time))
 
 if not initialized:
-    deploy_initialization()
+    check_and_initialize_encoding()
     initialized = True
-
 
 @app.route('/submit', methods=['POST'])
 def submit_data():
     try:
-        print('connected successfully')
+        print('Connected successfully')
 
         roll_no = request.form.get('roll_no')
-
         image_file = request.files.get('file')
 
         if not roll_no or not image_file:
