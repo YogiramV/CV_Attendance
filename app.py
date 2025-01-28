2af8d1dc72ed7424cae0db70ccc89c1b3c4717a5
YogiramV <yogiramkv@gmail.com>
2025-01-28 15:25:50 +0530

    Update loading encoding code

--- app.py
+++ app.py
@@ -4,14 +4,22 @@ import io
 
 known_face_encodings, known_face_rollnos, known_face_names = [], [], []
 
-def process_face_recognition(captured_image, rollno_input):
-    global known_face_encodings, known_face_rollnos, known_face_names
+encodings_loaded = False
 
-    known_face_encodings, known_face_rollnos, known_face_names = load_encodings()
+def process_face_recognition(captured_image, rollno_input):
+    global known_face_encodings, known_face_rollnos, known_face_names, encodings_loaded
+
+
+    if not encodings_loaded:
+        known_face_encodings, known_face_rollnos, known_face_names = load_encodings()
+        encodings_loaded = True
+    
+
 
     image_bytes = captured_image.getvalue()
     img = Image.open(io.BytesIO(image_bytes))
 
     result = scan_photo(img, rollno_input, known_face_encodings, known_face_rollnos, known_face_names)
+    
 
     return result
